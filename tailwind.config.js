ba018ddd1a79ae94f14d508b5206f3c308dc9157
sh1vrm <sanketh.mekala@edu.sait.ca>
2024-01-25 16:35:20 -0700

    header-css
    basic-css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      }
+  }
