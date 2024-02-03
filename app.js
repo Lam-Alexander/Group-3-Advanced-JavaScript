bbcd2c5801515fd948a33c33334fbacd495fb464
Jaden Whitman <141198734+Chiniseman@users.noreply.github.com>
2024-02-03 11:24:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ let lbstokilo = document.getElementById("lbstokilo"); lbstokilo.addEventListener
 
 let miletokilo = document.getElementById("miletokilo"); miletokilo.addEventListener('click', () => { type.innerHTML = 'Miles to Kilometers'; f = document.getElementById("input").value; Arraycalculation(f.split(","), 1.609344); });
 
-let CtoF = document.getElementById("CtoF"); CtoF.addEventListener('click', () => { type.innerHTML = 'Celcius to Fahrenheit'; f = document.getElementById("input").value; const output = f.split(","); for (let i = output.length - 1; i >= 0; i--) { output[i] = ((output[i] - 32) / 1.8).toFixed(3); calculation.innerHTML = output } });
+let CtoF = document.getElementById("CtoF"); CtoF.addEventListener('click', () => { type.innerHTML = 'Celcius to Fahrenheit'; f = document.getElementById("input").value; const output = f.split(","); for (let i = output.length - 1; i >= 0; i--) { output[i] = ((output[i] * 1.8) + 32).toFixed(3); calculation.innerHTML = output }});
 
 let kilotolbs = document.getElementById("kilotolbs"); kilotolbs.addEventListener('click', () => { type.innerHTML = 'Kilograms to Pounds'; f = document.getElementById("input").value; Arraycalculation(f.split(","), 2.204623); });
 
 let kilotomile = document.getElementById("kilotomile"); kilotomile.addEventListener('click', () => { type.innerHTML = 'Kilometers to Miles'; f = document.getElementById("input").value; Arraycalculation(f.split(","), 0.621371); });
 
-let FtoC = document.getElementById("FtoC"); FtoC.addEventListener('click', () => { type.innerHTML = 'Fahrenheit to Celcius'; f = document.getElementById("input").value; const output = f.split(","); for (let i = output.length - 1; i >= 0; i--) { output[i] = ((output[i] * 1.8) + 32).toFixed(3); calculation.innerHTML = output } });
+let FtoC = document.getElementById("FtoC"); FtoC.addEventListener('click', () => { type.innerHTML = 'Fahrenheit to Celcius'; f = document.getElementById("input").value; const output = f.split(","); for (let i = output.length - 1; i >= 0; i--) { output[i] = ((output[i] - 32) / 1.8).toFixed(3); calculation.innerHTML = output } });
 
 document.querySelectorAll('.sidePanel button').forEach(button => {
   button.onclick = () => {
